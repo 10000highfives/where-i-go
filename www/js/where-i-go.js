d33185692f30b5d5b505d7bb94cf6bba4f4b432f
10000highfives <getbusyonthedancefloor@hotmail.com>
2015-01-29 13:05:28 -0800

    created where-i-go.js
    added event listener to wait for Cordova to load.
    added script to check internet connection and redraw connection icon if no connection.

---
+++ www/js/where-i-go.js
@@ -0,0 +1,18 @@
+//where-i-go.js JavaScript file for where i go app
+
+document.addEventListener("deviceready", onDeviceReady);
+
+function onDeviceReady() {
+	checkConnection();
+	}
+
+function checkConnection() {
+	var networkState = navigator.network.connection.type;
+	
+	var states = {};
+	states[Connection.UNKNOWN]	= 'Unknown connection';
+	states[Connection.WIFI]		= 'WiFi connection';
+	states[Connection.CELL_3G]	= '3G connection';
+	states[Connection.CELL_4G]	= '4G connection';
+	states[Connection.NONE]		= 'no connection';
+	}
