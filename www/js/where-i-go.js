b13b274aacb997addcc07a20b917b0730595f60a
10000highfives <getbusyonthedancefloor@hotmail.com>
2015-01-31 07:41:12 -0800

    added tracking function incorporating Cordova's watchPosition() function.

--- www/js/where-i-go.js
+++ www/js/where-i-go.js
@@ -15,4 +15,30 @@ function checkConnection() {
 	states[Connection.CELL_3G]	= '3G connection';
 	states[Connection.CELL_4G]	= '4G connection';
 	states[Connection.NONE]		= 'no connection';
-	}
+	
+	if(states[networkState] == Connection.UNKNOWN){
+		$("#home_checkconnection").text('No Internet Access').attr("data-icon", "delete").button("refresh");
+		
+		}
+	};
+var track_id = ' ';
+var watch_id = null;
+var tracking_data= [];
+
+$("startTrackingStart").live('click', function() {
+	
+	watch_id = navigator.geolocation.watchPosition(
+		
+		function(position){
+			tracking_data.push(position);
+		},
+		
+		function(error){
+			console.log(error);
+			//instead of logging error, review inserting into the DOM via document.getElementByID("startTrackingDebug") and inner.html
+		}
+			
+		{frequency: 3000, enableHighAccuracy: true});
+	
+	$("startTrackingStatus").html("Now tracking");
+};
